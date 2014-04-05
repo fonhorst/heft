8d9c4f79a8b4faba9d507df3e75cdfe759cfd7ec
fonhorst <alipoov@rambler.ru>
2014-04-05 12:14:00 +0400

    Possibility to use parallel run on one machine added

--- core/comparisons/GaHeftOldPop.py
+++ core/comparisons/GaHeftOldPop.py
@@ -1,10 +1,13 @@
 from functools import partial
+from scoop import futures
 from core.runners.ExecutorRunner import ExecutorsFactory
-
+wf_name = "Montage_100"
+tsk_period = 5
+repeat_count = 2
 func = partial(ExecutorsFactory.default().run_gaheftoldpop_executor,
                                      reliability=0.95,
                                      is_silent=True,
-                                     wf_name="Montage_50",
+                                     wf_name=wf_name,
                                      logger=None,
                                      key_for_save='small_run',
                                      #task_id_to_fail="ID00005_000",
@@ -17,7 +20,26 @@ func = partial(ExecutorsFactory.default().run_gaheftoldpop_executor,
                                         "generations": 10
                                      })
 
-tasks_to_fail = ["ID00005_000"]
+def fnc(tsk):
+    return ExecutorsFactory.default().run_gaheftoldpop_executor(
+                                     reliability=0.95,
+                                     is_silent=True,
+                                     wf_name=wf_name,
+                                     logger=None,
+                                     key_for_save='small_run',
+                                     #task_id_to_fail="ID00005_000",
+                                     task_id_to_fail=tsk,
+                                     fixed_interval_for_ga=15,
+                                     ga_params={
+                                        "population": 50,
+                                        "crossover_probability": 0.8,
+                                        "replacing_mutation_probability": 0.5,
+                                        "sweep_mutation_probability": 0.4,
+                                        "generations": 10
+                                     })
+
+
+#tasks_to_fail = ["ID00005_000"]
 # tasks_to_fail = ["ID00010_000"]
 # tasks_to_fail = ["ID00015_000"]
 # tasks_to_fail = ["ID00020_000"]
@@ -40,10 +62,21 @@ tasks_to_fail = ["ID00005_000"]
 #==================================================
 # medium run
 #==================================================
-for t in tasks_to_fail:
-    for i in range(25):
-        print("Task: {0} Iter: {1}".format(t, i))
-        func(task_id_to_fail=t)
-        pass
-    pass
+# for t in tasks_to_fail:
+#     for i in range(25):
+#         print("Task: {0} Iter: {1}".format(t, i))
+#         func(task_id_to_fail=t)
+#         pass
+#     pass
 
+
+#==================================================
+# parallel run
+#==================================================
+tnum = int(wf_name.split("_")[1])
+tasks_to_fail = ["ID000{0}_000".format("0"+str(t) if t < 10 else str(t)) for t in range(0, tnum, tsk_period)]
+to_exec = [t for i in range(repeat_count) for t in tasks_to_fail]
+
+if __name__ == "__main__":
+    res = list(futures.map_as_completed(fnc, to_exec))
+    pass
