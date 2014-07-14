7afe7060a3c14ed1134a3b03710ab4156de211f5
fonhorst <alipoov@rambler.ru>
2014-07-14 15:22:37 +0400

    corrected setup file

--- setup.py
+++ setup.py
@@ -1,13 +1,19 @@
 from os.path import exists
 from setuptools import setup
 
-setup(name='heft',
-      version='0.1.1',
-      description='A static scheduling heuristic',
-      url='http://github.com/mroclin/heft',
-      author='Matthew Rocklin',
-      author_email='mrocklin@gmail.com',
+setup(name='SchedulerStand',
+      version='0.1',
+      description='scheduling heuristics for hpc environment',
+      url='https://github.com/fonhorst/heft',
+      author='fonhorst',
+      author_email='alipoov.nb@gmail.com',
       license='BSD',
       packages=['heft'],
       long_description=open('README.md').read() if exists("README.md") else "",
-      zip_safe=False)
+      zip_safe=False,
+      install_requires=['deap>=1.0.1',
+                        'numpy>=1.8.0',
+                        'scoop>=0.7',
+                        'networkx>=1.8.1',
+                        'numpy>=1.8.0',
+                        'distance>=0.1.3'])
