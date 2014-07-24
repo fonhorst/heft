86eb1c83339c1259c3c941868d999561922eb44c
fonhorst <alipoov.nb@gmail.com>
2014-07-24 09:00:00 -0400

    debuggable configuration for launch of cga_exp

--- heft/launcher.py
+++ heft/launcher.py
@@ -11,12 +11,17 @@ import sys
 from importlib import import_module
 
 if __name__ == "__main__":
+    #for arg in sys.argv:
+     #   print(arg)
     assert len(sys.argv) > 1, "qualified name for script hasn't been given"
-    assert os.path.exists(sys.argv[1]), "Path is not valid"
+    assert os.path.exists(sys.argv[-1]), "Path is not valid"
 
-    script_name = sys.argv[1]
+    script_name = sys.argv[-1]
     cur_dir = os.getcwd()
+
     assert script_name.startswith(cur_dir), "script doesn't lay in the directory hierarchy of the project"
+
+
     script_name = script_name[len(cur_dir):]
     script_name = script_name.replace("/", ".").strip(".")
     script_name = script_name[0:-3] if script_name.endswith(".py") else script_name
