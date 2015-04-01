3425d9a63e68f0b4e28dc96a7ef44ee7fac6d608
fonhorst <alipoov@gmail.com>
2015-04-01 04:25:08 +0400

    corrected aggregator

--- heft/experiments/aggregate_utilities.py
+++ heft/experiments/aggregate_utilities.py
@@ -1,6 +1,7 @@
 import json
 import os
 import math
+from pprint import pprint
 import matplotlib.pyplot as plt
 from heft.settings import TEMP_PATH
 from scipy import stats
@@ -56,7 +57,8 @@ def visualize(data, functions, path_to_save=None):
     plt.figure(figsize=(10, 10))
 
     for i in range(len(functions)):
-        plt.subplot(len(functions), 1, i + 1)
+        #plt.subplot(len(functions), 1, i + 1)
+        plt.clf()
         functions[i](data)
 
     plt.tight_layout()
@@ -103,12 +105,19 @@ class InMemoryDataAggregator:
             with open(p, "r") as f:
                 d = json.load(f)
                 self._data_array.append(d)
+
+        ## TODO: debug
+#        pprint(self._data_array)
         pass
 
     def __call__(self, picture_path="gh.png", extract_and_add=None, functions=None):
         data = {}
         for d in self._data_array:
             extract_and_add(data, d)
+
+        ## TODO: debug
+        pprint(data)
+
         path = os.path.join(TEMP_PATH, picture_path) if not os.path.isabs(picture_path) else picture_path
         visualize(data, functions, path)
         pass
