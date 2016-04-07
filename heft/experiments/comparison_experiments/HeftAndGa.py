e58ab9c3bbf74ffa7c2c806d89bfdc526cf36738
fonhorst <alipoov.nb@gmail.com>
2016-04-07 16:50:48 +0400

    old unknown changes

--- heft/experiments/comparison_experiments/HeftAndGa.py
+++ heft/experiments/comparison_experiments/HeftAndGa.py
@@ -1,6 +1,7 @@
 from copy import deepcopy
 import os
 import sys
+import datetime
 from heft.settings import RESOURCES_PATH
 import yaml
 from heft.algs.common.utilities import unzip_result, logbooks_in_data, data_to_file
@@ -60,15 +61,18 @@ class ParametrizedGaRunner:
 
         self._wf = wf(self._config.wf_name)
         # it's equal 10, 15, 25, 30 when ideal_flops == 1
-        self._rm = ExperimentResourceManager(rg.r([10, 15, 25, 30]))
+        # self._rm = ExperimentResourceManager(rg.r([10, 15, 25, 30, ]))
+        # self._rm = ExperimentResourceManager(rg.r([10, 15, 25, 30]*4))
+        # self._rm = ExperimentResourceManager(rg.r([10]*1 + [15]*2 + [25]*3 + [30]*2))
+        self._rm = ExperimentResourceManager(rg.r([10]*2 + [15]*3 + [25]*7 + [30]*4))
         # estimator = SimpleTimeCostEstimator(comp_time_cost=0, transf_time_cost=0, transferMx=None,
         #                                     ideal_flops=ideal_flops, transfer_time=100)
-        self._estimator = ExperimentEstimator(ideal_flops=20, transfer_nodes=100, transfer_blades=100)
+        # self._estimator = ExperimentEstimator(ideal_flops=20, transfer_nodes=100, transfer_blades=100)
         # transfer_nodes means now channel bandwidth
         # MB_100_CHANNEL = 100*1024*1024
-        # MB_100_CHANNEL = 7*1024*1024
-        # estimator = TransferCalcExperimentEstimator(ideal_flops=ideal_flops,
-        #                                             transfer_nodes=MB_100_CHANNEL, transfer_blades=100)
+        MB_100_CHANNEL = 100*1024*1024
+        self._estimator = TransferCalcExperimentEstimator(ideal_flops=20,
+                                                    transfer_nodes=MB_100_CHANNEL, transfer_blades=100)
         pass
 
     def do_run_heft(self):
@@ -78,6 +82,9 @@ class ParametrizedGaRunner:
         return heft_schedule
 
     def do_run_ga(self, initial_schedule):
+
+        start_time = datetime.datetime.now()
+
         def default_fixed_schedule_part(resource_manager):
             fix_schedule_part = Schedule({node: [] for node in HeftHelper.to_nodes(resource_manager.get_resources())})
             return fix_schedule_part
@@ -91,13 +98,18 @@ class ParametrizedGaRunner:
                        ga_params=self._config.ga_params)(fix_schedule_part, initial_schedule)
 
         self._validate(self._wf, self._estimator, schedule)
-        print("GA makespan: " + str(Utility.makespan(schedule)))
+
+        end_time = datetime.datetime.now()
+        length = end_time - start_time
+        print("GA makespan(sec): " + str(Utility.makespan(schedule)))
+        print("GA Time(sec):  " + str(length.seconds))
         return schedule, logbook
 
     # the func return the makespan of ga's schedule
     def __call__(self):
         heft_schedule = self.do_run_heft()
         ga_schedule, logbook = self.do_run_ga(initial_schedule=heft_schedule)
+        # ga_schedule, logbook = self.do_run_ga(initial_schedule=None)
         return (Utility.makespan(ga_schedule), logbook)
 
     def _validate(self, _wf, estimator, schedule):
@@ -115,7 +127,8 @@ if __name__ == '__main__':
     #    raise Exception("Path to config or folder with config is not found")
 
     # example of config is in resources folder: paramgarunner_example.yaml
-    cfg_path = "E:\\Melnik\\heft\\resources\\config"
+    # cfg_path = "E:\\Melnik\\heft\\resources\\config"
+    cfg_path = os.path.join(RESOURCES_PATH, "paramgarunner_example.yaml")
     if os.path.isdir(cfg_path):
         configs_to_be_executed = [os.path.join(cfg_path, el) for el in os.listdir(cfg_path)]
     else:
@@ -123,10 +136,10 @@ if __name__ == '__main__':
 
     configs_to_be_executed = [Config.load_from_file(cfg) for cfg in configs_to_be_executed]
 
-    repeat_count = 100
+    repeat_count = 1
     for config in configs_to_be_executed:
         runner = ParametrizedGaRunner(config)
         result, logbooks = unzip_result(repeat(runner, repeat_count))
         logbook = logbooks_in_data(logbooks)
-        data_to_file("./"+config.name+".txt", 300, logbook)
-        print(result)
+        #data_to_file("./"+config.name+".txt", 300, logbook)
+        #print(result)
