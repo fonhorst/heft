89789cec3c2927df80a5e73806aa793d9ec27f4c
fonhorst <alipoov.nb@gmail.com>
2016-04-07 17:43:20 +0400

    ga parameters (crossover, mutation, swap_mutation) investigation experiment

--- heft/experiments/comparison_experiments/HeftAndGa.py
+++ heft/experiments/comparison_experiments/HeftAndGa.py
@@ -1,7 +1,9 @@
 from copy import deepcopy
 import os
+from statistics import mean
 import sys
 import datetime
+import uuid
 from heft.settings import RESOURCES_PATH
 import yaml
 from heft.algs.common.utilities import unzip_result, logbooks_in_data, data_to_file
@@ -24,6 +26,10 @@ if scoop.IS_RUNNING:
 else:
     map_func = map
 
+CXPB = "crossover_probability"
+MUTPB = "replacing_mutation_probability"
+SWEEPMUTPB = "sweep_mutation_probability"
+
 CURRENT_CONFIG = None
 class Config(dict):
     @staticmethod
@@ -35,6 +41,37 @@ class Config(dict):
             cfg = yaml.load(f)
 
         return Config(cfg, name)
+
+    @staticmethod
+    def gaParamsInvestigationConfigs():
+
+        base_config = {
+            "ga_params": {
+                "Kbest": 50,
+                "population": 6,
+                CXPB: 0.1,
+                MUTPB: 0.1,
+                SWEEPMUTPB: 0.1,
+                "generations": 100
+            },
+            "wf_name": "Montage_25"
+        }
+
+        param_values = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
+
+        def buildGaParams(cxpb, mutpb, sweepmutpb):
+            config = deepcopy(base_config)
+            config["ga_params"][CXPB] = cxpb
+            config["ga_params"][MUTPB] = mutpb
+            config["ga_params"][SWEEPMUTPB] = sweepmutpb
+            return Config(config, "sweep_run")
+
+
+        gaParamsSets = [buildGaParams(cxpb, mutpb, sweepmutpb) for cxpb in param_values
+                    for mutpb in param_values
+                    for sweepmutpb in param_values]
+
+        return gaParamsSets
 
     def __init__(self, dct, name):
         super().__init__(dct)
@@ -61,18 +98,18 @@ class ParametrizedGaRunner:
 
         self._wf = wf(self._config.wf_name)
         # it's equal 10, 15, 25, 30 when ideal_flops == 1
-        # self._rm = ExperimentResourceManager(rg.r([10, 15, 25, 30, ]))
+        self._rm = ExperimentResourceManager(rg.r([10, 15, 25, 30, ]))
         # self._rm = ExperimentResourceManager(rg.r([10, 15, 25, 30]*4))
         # self._rm = ExperimentResourceManager(rg.r([10]*1 + [15]*2 + [25]*3 + [30]*2))
-        self._rm = ExperimentResourceManager(rg.r([10]*2 + [15]*3 + [25]*7 + [30]*4))
+        # self._rm = ExperimentResourceManager(rg.r([10]*2 + [15]*3 + [25]*7 + [30]*4))
         # estimator = SimpleTimeCostEstimator(comp_time_cost=0, transf_time_cost=0, transferMx=None,
         #                                     ideal_flops=ideal_flops, transfer_time=100)
-        # self._estimator = ExperimentEstimator(ideal_flops=20, transfer_nodes=100, transfer_blades=100)
+        self._estimator = ExperimentEstimator(ideal_flops=20, transfer_nodes=100, transfer_blades=100)
         # transfer_nodes means now channel bandwidth
         # MB_100_CHANNEL = 100*1024*1024
-        MB_100_CHANNEL = 100*1024*1024
-        self._estimator = TransferCalcExperimentEstimator(ideal_flops=20,
-                                                    transfer_nodes=MB_100_CHANNEL, transfer_blades=100)
+        # MB_100_CHANNEL = 100*1024*1024
+        # self._estimator = TransferCalcExperimentEstimator(ideal_flops=20,
+        #                                             transfer_nodes=MB_100_CHANNEL, transfer_blades=100)
         pass
 
     def do_run_heft(self):
@@ -121,25 +158,50 @@ class ParametrizedGaRunner:
 
 
 
+# if __name__ == '__main__':
+#
+#     #if len(sys.argv) != 2:
+#     #    raise Exception("Path to config or folder with config is not found")
+#
+#     # example of config is in resources folder: paramgarunner_example.yaml
+#     # cfg_path = "E:\\Melnik\\heft\\resources\\config"
+#     cfg_path = os.path.join(RESOURCES_PATH, "paramgarunner_example.yaml")
+#     if os.path.isdir(cfg_path):
+#         configs_to_be_executed = [os.path.join(cfg_path, el) for el in os.listdir(cfg_path)]
+#     else:
+#         configs_to_be_executed = [cfg_path]
+#
+#     configs_to_be_executed = [Config.load_from_file(cfg) for cfg in configs_to_be_executed]
+#
+#     repeat_count = 1
+#     for config in configs_to_be_executed:
+#         runner = ParametrizedGaRunner(config)
+#         result, logbooks = unzip_result(repeat(runner, repeat_count))
+#         logbook = logbooks_in_data(logbooks)
+#         #data_to_file("./"+config.name+".txt", 300, logbook)
+#         #print(result)
+
 if __name__ == '__main__':
 
-    #if len(sys.argv) != 2:
-    #    raise Exception("Path to config or folder with config is not found")
+    outFilepath = "D:/wspace/out_{0}_{1}.txt".format(datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S"),
+                                                     uuid.uuid4())
 
-    # example of config is in resources folder: paramgarunner_example.yaml
-    # cfg_path = "E:\\Melnik\\heft\\resources\\config"
-    cfg_path = os.path.join(RESOURCES_PATH, "paramgarunner_example.yaml")
-    if os.path.isdir(cfg_path):
-        configs_to_be_executed = [os.path.join(cfg_path, el) for el in os.listdir(cfg_path)]
-    else:
-        configs_to_be_executed = [cfg_path]
+    configSets = Config.gaParamsInvestigationConfigs()
 
-    configs_to_be_executed = [Config.load_from_file(cfg) for cfg in configs_to_be_executed]
-
-    repeat_count = 1
-    for config in configs_to_be_executed:
+    repeat_count = 10
+    for config in configSets:
         runner = ParametrizedGaRunner(config)
-        result, logbooks = unzip_result(repeat(runner, repeat_count))
-        logbook = logbooks_in_data(logbooks)
-        #data_to_file("./"+config.name+".txt", 300, logbook)
-        #print(result)
+        print("cxpb: {0}, mutpb: {1}, sweepmutpb: {2}".format(config.ga_params[CXPB],
+                                                              config.ga_params[MUTPB],
+                                                              config.ga_params[SWEEPMUTPB]))
+
+        makespans, logbooks = unzip_result(repeat(runner, repeat_count))
+        out_line = "{0}\t{1}\t{2}\t{3}\t{4}\n".format(config.ga_params[CXPB],
+                                               config.ga_params[MUTPB],
+                                               config.ga_params[SWEEPMUTPB],
+                                               mean(makespans),
+                                               makespans)
+        with open(outFilepath,"a") as out:
+            out.write(out_line)
+
+        print(makespans)
