2a31ac6a207bf5152307b6a1a14b889ff9d7317d
fonhorst <alipoov.nb@gmail.com>
2016-04-08 16:07:31 +0400

    experiments runs redistribution

--- heft/experiments/cga/utilities/common.py
+++ heft/experiments/cga/utilities/common.py
@@ -139,11 +139,11 @@ class ComparableMixin(object):
 
 def multi_repeat(n, funcs):
     if USE_SCOOP:
-        fs = [futures.submit(func) for func in funcs for _ in range(n)]
+        fs = [futures.submit(func) for _ in range(n) for func in funcs]
         futures.wait(fs)
         return [f.result() for f in fs]
     else:
-        return [func() for func in funcs for _ in range(n)]
+        return [func() for _ in range(n) for func in funcs ]
 
 def repeat(func, n):
     return multi_repeat(n, [func])
